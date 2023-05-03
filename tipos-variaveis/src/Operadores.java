cd46a4c69eaf7af1e270fca9128875018a95e19b
LucasFe97 <lucasferreirasd82@gmail.com>
2023-05-03 19:16:00 -0300

    Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,32 +1,88 @@
+import java.sql.Blob;
+
 public class Operadores {
     public static void main(String[] args) {
-         String concatenacao = "?";
+        String concatenacao = "?";
 
-         concatenacao = 1 + 1 + 1 + "1";
-         System.out.println(concatenacao);
-       
-         concatenacao = 1 + "1" + 1 + 1;
-         System.out.println(concatenacao);
-        
-         concatenacao = 1 + "1" + 1 + "1";
-         System.out.println(concatenacao);
-         
-         concatenacao = "1" + 1 + 1 + 1;
-         System.out.println(concatenacao);
-        
-         concatenacao = "1" + (1 + 1 + 1);
-         System.out.println(concatenacao);
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
 
-            //Unários
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+        // Unários
 
         int numero = 5;
 
-        numero = - numero;
-       
+        numero = -numero;
+
         System.out.println(numero);
 
         numero = numero * -1; // Para tornar um número positivo é necessário fazer uma multiplicação
         System.out.println(numero);
-    } 
+
+        // Unários Incremento e Decremento
+
+        int numeroExemplo = 5;
+
+        System.out.println(numeroExemplo++);
+        System.out.println(numeroExemplo);
+
+        // Unário negação
+
+        Boolean variavel = true;
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+        // Ternário
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+        
+
+         System.out.println(resultado);
+
+         //Boolean
+
+         int numero1 = 6;
+         int numero2 = 6;
+
+         boolean simNao = numero1 == numero2;
+
+         System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+        
+         simNao = numero1 > numero2;
+
+         System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+         // Operadores Lógicos (&&) (||)
+
+         boolean condicao1 = true;
+         
+         Boolean condicao2 = false;
+
+         if(condicao1 && condicao2){
+            System.out.println("as duas condiçoes são verdadeiras.");
+         }
+
+         if (condicao1 || condicao2){
+            System.out.println("Uma das condiçoes é verdadeira");
+         }
+
+    }
 
 }
--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -3,26 +3,26 @@ public class tiposVariaveis {
     
     public static void main(String[] args) {
         
-        // double salarioMinimo = 2500;
+        double salarioMinimo = 2500;
 
-        // TiposEVariaveis.java
+        //TiposEVariaveis.java
 
-        //  short numeroCurto = 1;
-        //  int numeroNormal = numeroCurto;
-        //  short numeroCurto2 = (short) numeroNormal;
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal;
 
-         // variaveis e constantes 
+        // variaveis e constantes 
 
-        //  int numero = 5;
+         int numero = 5;
         
-        // numero = 10;
+        numero = 10;
 
 
-        //  System.out.println(numero);
+         System.out.println(numero);
 
-        // final double VALOR_DE_PI = 3.14;
+        final double VALOR_DE_PI = 3.14;
 
-        // Final é usado para que um valor não possa ser alterado.
+        //Final é usado para que um valor não possa ser alterado.
     
     }
 }
